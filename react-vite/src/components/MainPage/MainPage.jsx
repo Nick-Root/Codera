4eb2ac7a4df093e0555c0a1704bfea4132f5ae0f
nick <nickroot168@gmail.com>
2023-12-17 21:15:53 -0500

    readability styling

--- react-vite/src/components/MainPage/MainPage.jsx
+++ react-vite/src/components/MainPage/MainPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { thunkGetAllQuestions } from "../../redux/question";
 import { thunkGetAllTopics } from "../../redux/topic";
 import { NavLink } from "react-router-dom";
+import './MainPage.css'
 
 const MainPage = () => {
     const dispatch = useDispatch();
@@ -15,31 +16,44 @@ const MainPage = () => {
     let questions = useSelector((state) => state.question)
     let topics = useSelector((state) => state.topic)
 
-    if (!questions, !topics ) return null
+    if (!questions, !topics) return null
     let arrQues = Object.values(questions)
     let arrTopic = Object.values(topics)
 
-        // console.log("%c   LOOK HERE", "color: red; font-size: 18px", arrQues);
+    // console.log("%c   LOOK HERE", "color: red; font-size: 18px", arrQues);
     return (
         <>
+            <div className='mainpage'>
 
-            <div className='questions-container'>
-                {arrQues.map((question) => (
-                    <div className='question' key={question.id}>{question.question}</div>
-                ))}
+                {/* All Topics */}
+                <div className='topicscont'>
+                    <button className='createtopic'>+  Create Topic</button> {/* placeholder for a create topic modal */}
+                    <h3 className='topicheader'>Topics</h3>
+                    <div className='topics'>
+                        {arrTopic.map((topic) => {
+                            return (
+                                <NavLink to={`/topics/${topic.id}`} key={topic.id} className='topic'>
+                                    {topic.topic}
+                                </NavLink>
+                            );
+                        })}
+                    </div>
+                </div>
+
+                <div className='questionscont'>
+                    <div className='askaques'>
+                        <p>Placeholder for ask a question box</p>
+                        <p>(will have a modal button on the nav aswell)</p>
+                    </div>
+                    {arrQues.map((question) => {
+                        return (
+                            <NavLink to={`/questions/${question.id}`} className='question' key={question.id}>{question.question}</NavLink>
+                        )
+                    })}
+                </div>
+
+
             </div>
-
-            {/* All Topics */}
-            <div>
-                {arrTopic.map((topic) => {
-                    return (
-                        <NavLink to={`/topics/${topic.id}`} key={topic.id}>
-                            {topic.topic}
-                        </NavLink>
-                    );
-                })}
-            </div>
-
         </>
 
     )
