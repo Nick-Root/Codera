f65cab1ee885ed19381a098995fbebef7fe69144
nick <nickroot168@gmail.com>
2023-12-21 14:19:25 -0500

    splash page

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -76,99 +76,101 @@ function SignupFormPage() {
 
   return (
     <div className='splash-container'>
-      <div className="splash-form">
-        <h2>Sign Up</h2>
-        {errors.server && <p>{errors.server}</p>}
-        <form onSubmit={handleSubmit}>
-          <label>
-            First Name
-            <input
-              type="text"
-              value={firstname}
-              onChange={(e) => setFirstname(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Last Name
-            <input
-              type="text"
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Email
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          {errors.email && <p>{errors.email}</p>}
-          <label>
-            Username
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </label>
-          {errors.username && <p>{errors.username}</p>}
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          {errors.password && <p>{errors.password}</p>}
-          <label>
-            Confirm Password
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </label>
-          {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-          <button type="submit">Sign Up</button>
-        </form>
-      </div>
-      <div className="splash-form">
-        <h2>Log In</h2>
-        {errors.length > 0 &&
-          errors.map((message) => <p key={message}>{message}</p>)}
-        <form onSubmit={handleLoginSubmit}>
-          <label>
-            Email
-            <input
-              type="text"
-              value={email2}
-              onChange={(e) => setEmail2(e.target.value)}
-              required
-            />
-          </label>
-          {errors.email && <p>{errors.email}</p>}
-          <label>
-            Password
-            <input
-              type="password"
-              value={password2}
-              onChange={(e) => setPassword2(e.target.value)}
-              required
-            />
-          </label>
-          {errors.password && <p>{errors.password}</p>}
-          <button type="submit">Log In</button>
-        </form>
-        <button onClick={demo} style={{ cursor: 'pointer' }}>Demo User</button>
+      <div className="white-space">
+        <div className="splash-form-signup">
+          <h2 className='formhead'>Sign Up</h2>
+          {errors.server && <p>{errors.server}</p>}
+          <form onSubmit={handleSubmit}>
+            <label>
+              First Name
+              <input
+                type="text"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Last Name
+              <input
+                type="text"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Email
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            {errors.email && <p>{errors.email}</p>}
+            <label>
+              Username
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            {errors.username && <p>{errors.username}</p>}
+            <label>
+              Password
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            {errors.password && <p>{errors.password}</p>}
+            <label>
+              Confirm Password
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </label>
+            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+            <button type="submit" className="signup">Sign Up</button>
+          </form>
+        </div>
+        <div className="splash-form-login">
+          <h2 className='formhead'>Log In</h2>
+          {errors.length > 0 &&
+            errors.map((message) => <p key={message}>{message}</p>)}
+          <form onSubmit={handleLoginSubmit}>
+            <label>
+              Email
+              <input
+                type="text"
+                value={email2}
+                onChange={(e) => setEmail2(e.target.value)}
+                required
+              />
+            </label>
+            {errors.email && <p>{errors.email}</p>}
+            <label>
+              Password
+              <input
+                type="password"
+                value={password2}
+                onChange={(e) => setPassword2(e.target.value)}
+                required
+              />
+            </label>
+            {errors.password && <p>{errors.password}</p>}
+            <button type="submit" className="login">Log In</button>
+          </form>
+          <button className="demouser" onClick={demo} style={{ cursor: 'pointer' }}>Demo User</button>
+        </div>
       </div>
     </div>
   );
